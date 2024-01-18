679351db836f53255cfedba2b5a98440e0e73ac9
soka1998 <soukainaharifi18@gmail.com>
2024-01-18 16:18:30 +0000

    Added static method 'login' to userSchema for user authentication.

--- Models/User.js
+++ Models/User.js
@@ -31,3 +31,20 @@ userSchema.pre('save', async function (next) {
     // const salt = await bcrypt.hash(this.password,salt)
     next()
   })
+
+  //static method to login user
+userSchema.static.login = async function (email, password) {
+    const user = await this.findOne({ email })
+    if (user) {
+      const auth = await bcrypt.compare(password, user.password)
+      if (auth) {
+        return user
+      }
+      throw Error('incorrect password')
+    }
+    throw Error('incorrect email')
+  }
+  
+  const User = mongoose.model('user', userSchema)
+  module.exports = User
+  
