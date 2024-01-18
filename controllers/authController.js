5b662aa1bbb607f810957ecabbcf745b02a08ed0
soka1998 <soukainaharifi18@gmail.com>
2024-01-18 16:04:03 +0000

    Handle user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,3 +48,15 @@ module.exports.signup_post = async (req, res) => {
     res.status(400).json(errors)
   }
 }
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const user = await User.login(email, password)
+    const token = createToken(user._id)
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+    res.status(200).json({ user: user._id })
+  } catch (err) {
+    const errors = handleErrors(err)
+    res.status(400).json({ errors })
+  }
+}
