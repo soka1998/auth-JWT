0a24e200d66b858229386aa8f24f2836e905da0c
soka1998 <soukainaharifi18@gmail.com>
2024-01-18 16:40:20 +0000

    Added middleware 'checkUser' to verify and retrieve the current user information based on the JWT stored in cookies.

--- middleware/authMidlleware.js
+++ middleware/authMidlleware.js
@@ -20,3 +20,30 @@ const requireAuth = (req, res, next) => {
   }
   
 }
+  // Check current user
+  const checkUser =(req,res,next)
+  const token = req.cookies.jwt;
+
+  if(token){
+    jwt.verify(token, SECRET - KEY,async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message)
+        res.locals.user = null;
+        next();
+      } else {
+        console.log(decodedToken)
+        let user = await User.findById(decodedToken.id)
+        res.locals.user=user;
+        next()
+      }
+    })
+
+  }else{
+   res.locals.user = null;
+   next()
+
+  }
+
+
+
+module.exports = { requireAuth ,checkUser}
